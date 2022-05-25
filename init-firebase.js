24f8c253f064d94fb4d8f277be698aeff1a3abce
PixelHUN <59181913+PixelHUN@users.noreply.github.com>
2022-05-25 18:02:38 +0200

    asd?

---
+++ init-firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCBUBcmS9qBoVVsAXguJkmjB0cijRueTI8",
+  authDomain: "global-warm-thing.firebaseapp.com",
+  databaseURL: "https://global-warm-thing-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "global-warm-thing",
+  storageBucket: "global-warm-thing.appspot.com",
+  messagingSenderId: "344052791538",
+  appId: "1:344052791538:web:f55763742ae885a4e7786b"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
