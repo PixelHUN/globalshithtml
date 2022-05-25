9efee77f5cf3754c87dae7ead24850966ac7abdf
PixelHUN <59181913+PixelHUN@users.noreply.github.com>
2022-05-25 23:24:34 +0200

    scale fix?

--- game.js
+++ game.js
@@ -4,6 +4,10 @@ function randomFromArray(array)
 }
 
 (function(){
+  var siteWidth = 1280;
+  var scale = screen.width /siteWidth;
+
+  document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
   let playerId;
   let playerRef;
   let world;
