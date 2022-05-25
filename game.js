97703f42a889e34ff7081cc1a2a8a592d126aee9
PixelHUN <59181913+PixelHUN@users.noreply.github.com>
2022-05-25 23:01:06 +0200

    Játékosnév és lobby

--- game.js
+++ game.js
@@ -4,7 +4,6 @@ function randomFromArray(array)
 }
 
 (function(){
-
   let playerId;
   let playerRef;
   let world;
@@ -16,80 +15,92 @@ function randomFromArray(array)
 
   const gameContainer = document.querySelector(".game-container");
 
-  firebase.auth().onAuthStateChanged((user) => {
-    console.log(user)
-    if(user)
-    {
-      // wooo van felhasználó
+  let username = "";
+  document.getElementById("name-button").onclick = function() {EnterName()};
 
-      playerId = user.uid;
-      playerRef = firebase.database().ref(`players/${playerId}`);
-      world = firebase.database().ref(`world`);
+  function EnterName()
+  {
+    username = document.getElementById("nameinput").value;
+    firebase.auth().signInAnonymously().catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ...
+      console.log(errorCoded, errorMessage);
+    })
 
-      playerRef.set({
-        id: playerId,
-        host: 0,
-        uname: "hon't",
-        profile: "hetzmann-hont",
-        money: 1
-      })
-      // N.America, S. America, Europe, Africa, Asia, Australia
-      world.set({
-        year: 2022,
-        NAmerica: {
-          CO: 0,
-          Temperature: 0,
-          Happyness: 0,
-          Wealthyness: 0
-        },
-        SAmerica: {
-          CO: 0,
-          Temperature: 0,
-          Happyness: 0,
-          Wealthyness: 0
-        },
-        Europe: {
-          CO: 0,
-          Temperature: 0,
-          Happyness: 0,
-          Wealthyness: 0
-        },
-        Asia: {
-          CO: 0,
-          Temperature: 0,
-          Happyness: 0,
-          Wealthyness: 0
-        },
-        Africa: {
-          CO: 0,
-          Temperature: 0,
-          Happyness: 0,
-          Wealthyness: 0
-        },
-        Australia: {
-          CO: 0,
-          Temperature: 0,
-          Happyness: 0,
-          Wealthyness: 0
-        }
-      })
+    firebase.auth().onAuthStateChanged((user) => {
+      console.log(user)
+      if(user)
+      {
+        // wooo van felhasználó
 
-      playerRef.onDisconnect().remove();
+        playerId = user.uid;
+        playerRef = firebase.database().ref(`players/${playerId}`);
+        world = firebase.database().ref(`world`);
 
-      initGame();
-    }
-    else
-    {
-      // nincs több felhasználó
-    }
-  })
+        document.querySelector(".give-name").style.display="none";
+        document.querySelector(".character-name").innerText = "Dőlj hátra! Hamarosan kezdődik a játék.";
 
-  firebase.auth().signInAnonymously().catch((error) => {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // ...
-    console.log(errorCoded, errorMessage);
-  })
+        playerRef.set({
+          id: playerId,
+          host: 0,
+          uname: username,
+          profile: "hetzmann-hont",
+          money: 1
+        })
+        // N.America, S. America, Europe, Africa, Asia, Australia
+        world.set({
+          year: 2022,
+          NAmerica: {
+            CO: 0,
+            Temperature: 0,
+            Happyness: 0,
+            Wealthyness: 0
+          },
+          SAmerica: {
+            CO: 0,
+            Temperature: 0,
+            Happyness: 0,
+            Wealthyness: 0
+          },
+          Europe: {
+            CO: 0,
+            Temperature: 0,
+            Happyness: 0,
+            Wealthyness: 0
+          },
+          Asia: {
+            CO: 0,
+            Temperature: 0,
+            Happyness: 0,
+            Wealthyness: 0
+          },
+          Africa: {
+            CO: 0,
+            Temperature: 0,
+            Happyness: 0,
+            Wealthyness: 0
+          },
+          Australia: {
+            CO: 0,
+            Temperature: 0,
+            Happyness: 0,
+            Wealthyness: 0
+          }
+        })
+
+        playerRef.onDisconnect().remove();
+
+        initGame();
+      }
+      else
+      {
+        // nincs több felhasználó
+      }
+    })
+
+    console.log(username);
+  }
 
   function initGame(){
     const allPlayersRef = firebase.database().ref(`players`);
@@ -104,6 +115,7 @@ function randomFromArray(array)
       {
         players[playerId].host = 2;
         playerRef.set(players[playerId]);
+
       }
       if(snapshot.numChildren()===2 && players[playerId].host != 2)
       {
@@ -119,17 +131,20 @@ function randomFromArray(array)
         document.getElementById("button-host").style.display = "none";
       }
 
-      const index = playerNames.indexOf(snapshot.val().uname);
-      if (index > -1) {
-        playerNames.splice(index, 1); // 2nd parameter means remove one item only
-      }
-      document.querySelector(".character-name").innerText = playerNames;
-
-      const changedPlayer = snapshot.val();
-      if(changedPlayer.host != 1)
+      if(players[playerId].host === 2)
       {
-        playerNames.push(changedPlayer.uname);
-        document.querySelector(".character-name").innerText = changedNames;
+        const index = playerNames.indexOf(snapshot.val().uname);
+        if (index > -1) {
+          playerNames.splice(index, 1);
+        }
+        document.querySelector(".character-name").innerText = playerNames;
+
+        const changedPlayer = snapshot.val();
+        if(changedPlayer.host != 1)
+        {
+          playerNames.push(changedPlayer.uname);
+          document.querySelector(".character-name").innerText = changedNames;
+        }
       }
     })
 
@@ -152,7 +167,7 @@ function randomFromArray(array)
       {
         const index = playerNames.indexOf(snapshot.val().uname);
         if (index > -1) {
-          playerNames.splice(index, 1); // 2nd parameter means remove one item only
+          playerNames.splice(index, 1);
         }
         document.querySelector(".character-name").innerText = playerNames;
       }
